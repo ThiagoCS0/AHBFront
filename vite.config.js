9d9b64bfcd545aed06242efcd23c1356bde1e05f
Thiago C Silva <tkreserva@gmail.com>
2024-11-27 22:24:17 -0300

    fazendo deploy

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/AHBFront/"
-  // routes: [
-  //   { name: 'criar', path: '/criar', component: () => import('./src/Componentes/Acesso/Criar/Criar'), },
-  //   { name: 'recuperar', path: '/recuperar', component: () => import('./src/Componentes/Acesso/Recuperar/Recuperar'), },
-  //   {
-  //     name: 'acesso', path: '/', children: [
-  //       { name: 'criar', path: 'criar', component: () => import('./src/Componentes/Acesso/Criar/Criar'), },
-  //       { name: 'recuperar', path: 'recuperar', component: () => import('./src/Componentes/Acesso/Recuperar/Recuperar'), },
-  //     ],
-  //   }],
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: "/AHBFront/"
+  // server: {
+  //   port: 5173,
+  //   open: true,
+  // },
 })
